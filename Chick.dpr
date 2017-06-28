3ed2a4cd4f3145328262a4972ea2ed316bde042a
CryptoRamik <registrcx@mail.ru>
2017-06-28 13:21:39 +0300

    Add files via upload

---
+++ Chick.dpr
@@ -0,0 +1,13 @@
+program Chick;
+
+uses
+  Forms,
+  Chiken in 'Chiken.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
